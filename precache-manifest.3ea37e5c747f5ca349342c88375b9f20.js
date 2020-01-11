8bfbe44697b183ec01276b4972df44632c421266
Garefen <gabrielbsarti@gmail.com>
2020-01-11 01:35:43 -0300

    Updates

---
+++ precache-manifest.3ea37e5c747f5ca349342c88375b9f20.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9fa9dedfc58f9f5f82ed141f6343898f",
+    "url": "/weather/index.html"
+  },
+  {
+    "revision": "4ca564d2f7b972ae22a9",
+    "url": "/weather/static/css/main.f1fb2a17.chunk.css"
+  },
+  {
+    "revision": "1992e7060e1a3929b60e",
+    "url": "/weather/static/js/2.1439f25c.chunk.js"
+  },
+  {
+    "revision": "c6d01cb19213691bc81a274434d00fe7",
+    "url": "/weather/static/js/2.1439f25c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "4ca564d2f7b972ae22a9",
+    "url": "/weather/static/js/main.4dad44df.chunk.js"
+  },
+  {
+    "revision": "aedb6f7ea6a26810d0e2",
+    "url": "/weather/static/js/runtime-main.cb172771.js"
+  }
+]);
