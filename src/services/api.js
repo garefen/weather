5202563aeccc1ebeb2f0a13c21208a5b7a49428d
Garefen <gabrielbsarti@gmail.com>
2020-01-21 13:15:16 -0300

    api request changed to https

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-    baseURL: 'http://api.openweathermap.org/data/2.5/forecast?id=3448433&APPID=8f34ff6349e06c9ef90bb986c86ab9b4'
+    baseURL: 'https://api.openweathermap.org/data/2.5/forecast?id=3448433&APPID=8f34ff6349e06c9ef90bb986c86ab9b4'
 });
 
 export default api;
